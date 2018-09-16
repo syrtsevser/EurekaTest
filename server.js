f4312b3fdb0b9adf923100308e1ca8e7c4fe5b7f
Sergey Syrtsev <43320277+syrtsevser@users.noreply.github.com>
2018-09-16 23:00:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-﻿const express = require('express');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const usergroup = require('./build/Release/usergroup.node');
-
-/*
-var func = usergroup.usergroup_init('Пользователи');
-console.log(func);
-*/
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded( {extended: false} ));
@@ -18,8 +13,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req,res) => {
-	func = usergroup.usergroup_init(req.body.gName);
+	var func = usergroup.usergroup_init(req.body.gName);
 	res.send(func);
 });
 
-const listener = app.listen(20000, '127.0.0.1', () => console.log('Сервер запущен. Адрес: '+listener.address().address+':'+listener.address().port));
+const listener = app.listen(20000, '127.0.0.1', () =>
+	console.log('Сервер запущен. Адрес: '+listener.address().address+':'+listener.address().port));
